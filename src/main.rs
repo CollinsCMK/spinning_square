c5b58b295731ca252f6d20d57ca6f7e3d5f1a114
collinsCMK <collinskariuki555@gmail.com>
2024-06-17 18:46:32 +0300

    CMK spinning square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use draw::App;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::{window::WindowSettings, event_loop::{EventSettings, Events}, input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent}};
+
+mod draw;
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new("Spinning Square", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+    };
+
+    let mut events = Events::new(EventSettings::new());
+
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
+    }
 }
